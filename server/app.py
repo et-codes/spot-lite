2559409540eecb368dd4283b7271737f7d33cedd
et-codes <104942211+et-codes@users.noreply.github.com>
2022-12-09 12:07:08 -0700

    Remove flask-restful, use Flask route instead.

--- server/app.py
+++ server/app.py
@@ -1,33 +1,22 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
-import logging
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger(__name__)
-
 load_dotenv()
 
 app = Flask(__name__)
-api = Api(app)
+auth = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth)
 
-class Search(Resource):
-    def __init__(self):
-        auth = SpotifyClientCredentials()
-        self.sp = spotipy.Spotify(auth_manager=auth)
-
-    def get(self, search_term) -> None:
-        try:
-            result = self.sp.search(q=search_term, type="album", limit=1, market="US")
-        except Exception as err:
-            logger.error(f'{err=}')
-            return f'ERROR: {err=}', 500
-        return result, 200
-
-api.add_resource(Search, '/api/search/<string:search_term>')
+@app.route('/api/search/<string:search_term>')
+def get_search_results(search_term) -> None:
+    try:
+        result = sp.search(q=search_term, type="album", limit=1, market="US")
+    except Exception as err:
+        return f'ERROR: {err=}', 500
+    return result, 200
 
 
 if __name__ == '__main__':
